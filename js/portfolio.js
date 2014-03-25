8d06822f09cfd423a2bfea81f07683b26da27642
MattGelfand <matthew.gelfand@gmail.com>
2014-03-25 06:09:41 -0500

    add sidebar, contact page, new main image, hover states

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,14 +1,30 @@
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
+$(document).ready(function() {
 
-	    var target = this.hash,
-	    $target = $(target);
+  // set all main panel to window height
+  size_panels();
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+  // reset panel height on resize
+  $(window).on('resize', size_panels);
+
+  function size_panels(){
+    $('body > div').css('height', window.innerHeight);
+  }
+
+  $("li a").on('click', function(e) {
+    e.preventDefault();
+
+    var target = $(this).attr("href");
+
+    $("html, body").stop().animate({
+      scrollLeft: $(target).offset().left,
+      scrollTop: $(target).offset().top
+    }, 500);
+
+  });
+
+
+
+
+
+
 });
